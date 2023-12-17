541453d182e93b9b79c54b4ced221f1da4a625ec
beets_ftb <beetsftb@gmail.com>
2023-12-17 11:55:33 -0300

    Cambios para minimized js

--- js/cultura.js
+++ js/cultura.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const accordionButtons = document.querySelectorAll('.accordion-button-cultura');
-
-    accordionButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const content = this.parentElement.nextElementSibling;
-            content.style.display = content.style.display === 'block' ? 'none' : 'block';
-            this.textContent = this.textContent === '+' ? '-' : '+';
-        });
-    });
-});
+document.addEventListener("DOMContentLoaded",function(){let t=document.querySelectorAll(".accordion-button-cultura");t.forEach(t=>{t.addEventListener("click",function(){let t=this.parentElement.nextElementSibling;t.style.display="block"===t.style.display?"none":"block",this.textContent="+"===this.textContent?"-":"+"})})});
